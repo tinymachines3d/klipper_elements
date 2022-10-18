7aa57c9ab9330918bcb417c3a01deb6961fdbe32
TM3D <sales.tinymachines3d@gmail.com>
2022-10-18 18:47:42 -0500

    Update change-hostname.sh

--- troodon/scripts/change-hostname.sh
+++ troodon/scripts/change-hostname.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./home/pi/klipper_config/elements/troodon/scripts/change-hostname-as-root.sh $1
+sudo sh /home/pi/klipper_config/elements/troodon/scripts/change-hostname-as-root.sh $1
