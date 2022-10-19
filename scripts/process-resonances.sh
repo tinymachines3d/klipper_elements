90c73c121ea967dd3aa4aaf6e40078dea3181869
tinymachines3d <sales.tinymachines3d@gmail.com>
2022-10-19 02:05:49 -0500

    fix syntax

--- scripts/process-resonances.sh
+++ scripts/process-resonances.sh
@@ -4,10 +4,10 @@ now=$(date +"%F-%H_%M_%S")
 # TEST_RESONANCES AXIS=Y
 
 echo processing x-axis
-/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/input_shaper_results/shaper_calibrate_x-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/elements/troodon/input_shaper_results/shaper_calibrate_x-${now}.png
 
 echo processing y-axis
-/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/input_shaper_results/shaper_calibrate_y-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/elements/troodon/input_shaper_results/shaper_calibrate_y-${now}.png
 
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
