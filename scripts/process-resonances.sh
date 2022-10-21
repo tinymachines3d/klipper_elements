8b7749b3cf6b93c7351fcb5a0d0ca88f4da9d2d4
TM3D <chris@tinymachines3d.com>
2022-10-20 22:19:56 -0500

    fix file path

--- scripts/process-resonances.sh
+++ scripts/process-resonances.sh
@@ -4,13 +4,13 @@ now=$(date +"%F-%H_%M_%S")
 # TEST_RESONANCES AXIS=Y
 
 echo processing x-axis
-/home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /home/pi/klipper_config/elements/troodon/input_shaper_results/shaper_calibrate_x-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /home/pi/klipper_config/elements/input_shaper_results/shaper_calibrate_x-${now}.png
 
 echo processing y-axis
-/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/elements/troodon/input_shaper_results/shaper_calibrate_y-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/elements/input_shaper_results/shaper_calibrate_y-${now}.png
 
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
 # ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
-mv /tmp/resonances_*.csv /home/pi/klipper_config/elements/troodon/input_shaper_results
+mv /tmp/resonances_*.csv /home/pi/klipper_config/elements/input_shaper_results
 
