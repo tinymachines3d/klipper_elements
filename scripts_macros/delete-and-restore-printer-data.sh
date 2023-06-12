71d4ce85f42b707982a5e23a170652ce8e411d9b
TM3D <chris@tinymachines3d.com>
2023-06-12 10:51:35 -0500

    update

--- scripts_macros/delete-and-restore-printer-data.sh
+++ scripts_macros/delete-and-restore-printer-data.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ -d "/home/pi/printer_data" ]; then
+if [ -d "/home/biqu/printer_data" ]; then
   echo "Deleting printer data config, database, gcodes and logs directories.."
-  rm -rf /home/pi/printer_data/config
-  rm -rf /home/pi/printer_data/database
-  rm -rf /home/pi/printer_data/gcodes
-  rm -rf /home/pi/printer_data/logs
+  rm -rf /home/biqu/printer_data/config
+  rm -rf /home/biqu/printer_data/database
+  rm -rf /home/biqu/printer_data/gcodes
+  rm -rf /home/biqu/printer_data/logs
 else
-  mkdir /home/pi/printer_data
+  mkdir /home/biqu/printer_data
 fi
 
 echo "Restoring printer data config, database, gcodes and logs directories.."
-ln -s /home/pi/klipper_config /home/pi/printer_data/config
-ln -s /home/pi/.moonraker_database /home/pi/printer_data/database
-ln -s /home/pi/gcode_files /home/pi/printer_data/gcodes
-ln -s /home/pi/klipper_logs /home/pi/printer_data/logs
+ln -s /home/biqu/klipper_config /home/biqu/printer_data/config
+ln -s /home/biqu/.moonraker_database /home/biqu/printer_data/database
+ln -s /home/biqu/gcode_files /home/biqu/printer_data/gcodes
+ln -s /home/biqu/klipper_logs /home/biqu/printer_data/logs
