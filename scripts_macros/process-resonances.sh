74bb3c3b3cb2da36217421423f87302579b17bcf
TM3D <chris@tinymachines3d.com>
2023-06-30 10:38:23 -0500

    update scripts

--- scripts_macros/process-resonances.sh
+++ scripts_macros/process-resonances.sh
@@ -4,13 +4,13 @@ now=$(date +"%F-%H_%M_%S")
 # TEST_RESONANCES AXIS=Y
 
 echo processing x-axis
-/home/biqu/klipper/scripts_macros/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /home/biqu/printer_data/config/elements/input_shaping/input_shaper_results/shaper_calibrate_x-${now}.png
+/home/biqu/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /home/biqu/printer_data/config/elements/input_shaping/input_shaper_results/shaper_calibrate_x-${now}.png
 
 echo processing y-axis
-/home/biqu/klipper/scripts_macros/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/biqu/printer_data/config/elements/input_shaping/input_shaper_results/shaper_calibrate_y-${now}.png
+/home/biqu/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/biqu/printer_data/config/elements/input_shaping/input_shaper_results/shaper_calibrate_y-${now}.png
 
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
-# ~/klipper/scripts_macros/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
+# ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
 mv /tmp/resonances_*.csv /home/biqu/printer_data/config/elements/input_shaping/input_shaper_results
 
