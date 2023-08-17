02a52345085f1c6548b5ee158685f0d13ac8ecdb
TM3D <sales.tinymachines3d@gmail.com>
2023-08-17 14:37:29 -0500

    Update git-stash-elements-repo.sh

--- scripts_macros/git-stash-elements-repo.sh
+++ scripts_macros/git-stash-elements-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Change directory to /home/biqu/printer_data/config/elements
+# Change directory to /home/pi/printer_data/config/elements
 cd /home/pi/printer_data/config/elements
 
 # Run git stash
